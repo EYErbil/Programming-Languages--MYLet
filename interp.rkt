f65299d1c487b5a5f74ff83976700d70712bcfb5
Öykü Erdem <oykuerdem22@ku.edu.tr>
2026-04-15 16:49:31 +0300

    part d 4-6 implemented

--- interp.rkt
+++ interp.rkt
@@ -33,86 +33,84 @@
       (op-exp (exp1 exp2 op)
               (let ((val1 (value-of exp1 env))
                     (val2 (value-of exp2 env)))
-                  (let ((num1 (expval->rational val1))
-                        (num2 (expval->rational val2)))
+                (let ((num1 (expval->rational val1))
+                      (num2 (expval->rational val2)))
+                  (cond 
+                    ((and (number? num1) (number? num2))
+                     (num-val
                       (cond 
-                        ((and (number? num1) (number? num2))
-                          (num-val
-                            (cond 
-                              ((= op 1) (+ num1 num2))
-                              ((= op 2) (* num1 num2))
-                                    ;; -----------------------
-                                    ;; INSERT YOUR CODE HERE 
-                                    ;; -----------------------
+                        ((= op 1) (+ num1 num2))
+                        ((= op 2) (* num1 num2))
+                        ;; -----------------------
+                        ;; INSERT YOUR CODE HERE 
+                        ;; -----------------------
+                        ((= op 3) (if (zero? num2) (eopl:error 'value-of "Zero Div") (/ num1 num2))) 
+                        (else (- num1 num2))))) 
 
+                    ;; -----------------------
+                    
+                    ((and (number? num1) (not (number? num2)))
+                     (rational-val
+                      (let ((num2top (car num2))
+                            (num2bot (cdr num2)))
+                        (cond 
+                          ((= op 1) (cons (+ (* num1 num2bot) num2top) num2bot))
+                          ((= op 2) (cons (* num1 num2top) num2bot))
+                          ;; -----------------------
+                          ;; INSERT YOUR CODE HERE 
+                          ;; -----------------------
+                          ((= op 3) (if (zero? num2top) (eopl:error 'value-of "Zero Div") (cons (* num1 num2bot) num2top)))
+                          (else (cons (- (* num1 num2bot) num2top) num2bot))))))
 
-                                    ;; -----------------------
-                              )))
-                        
-                        ((and (number? num1) (not (number? num2)))
-                          (rational-val
-                          (let ((num2top (car num2))
-                                (num2bot (cdr num2)))
-                            (cond 
-                              ((= op 1) (cons (+ (* num1 num2bot) num2top) num2bot))
-                              ((= op 2) (cons (* num1 num2top) num2bot))
-                              ;; -----------------------
-                              ;; INSERT YOUR CODE HERE 
-                              ;; -----------------------
+                    ;; -----------------------
+                    
+                    ((and (number? num2) (not (number? num1)))
+                     (rational-val
+                      (let ((num1top (car num1))
+                            (num1bot (cdr num1)))
+                        (cond 
+                          ((= op 1) (cons (+ (* num1bot num2) num1top) num1bot))
+                          ((= op 2) (cons (* num1top num2) num1bot))
+                          ;; -----------------------
+                          ;; INSERT YOUR CODE HERE 
+                          ;; -----------------------
+                          ((= op 3) (if (zero? num2) (eopl:error 'value-of "Zero Div") (cons num1top (* num1bot num2))))
+                          (else (cons (- num1top (* num2 num1bot)) num1bot))))))
 
+                    ;; -----------------------
+                    
+                    (else
+                     (rational-val
+                      (let ((num1top (car num1))
+                            (num1bot (cdr num1))
+                            (num2top (car num2))
+                            (num2bot (cdr num2)))
+                        (cond 
+                          ((= op 1) (cons (+ (* num1top num2bot) (* num1bot num2top)) (* num1bot num2bot))) ;; add
+                          ((= op 2) (cons (* num1top num2top) (* num1bot num2bot))) ;; multiply
+                          ;; -----------------------
+                          ;; INSERT YOUR CODE HERE 
+                          ;; -----------------------
+                          ((= op 3) (if (zero? num2top) (eopl:error 'value-of "Zero Div") (cons (* num1top num2bot) (* num1bot num2top))))
+                          (else (cons (- (* num1top num2bot) (* num1bot num2top)) (* num1bot num2bot)))))))))))
 
-                              ;; -----------------------
+      ;; ----------------------- 
 
-                              
-                              ))))
-
-                        ((and (number? num2) (not (number? num1)))
-                          (rational-val
-                          (let ((num1top (car num1))
-                                (num1bot (cdr num1)))
-                            (cond 
-                              ((= op 1) (cons (+ (* num1bot num2) num1top) num1bot))
-                              ((= op 2) (cons (* num1top num2) num1bot))
-                              ;; -----------------------
-                              ;; INSERT YOUR CODE HERE 
-                              ;; -----------------------
-
-
-                              ;; -----------------------
-                              ))))
-
-                        (else
-                          (rational-val
-                          (let ((num1top (car num1))
-                                (num1bot (cdr num1))
-                                (num2top (car num2))
-                                (num2bot (cdr num2)))
-                            (cond 
-                              ((= op 1) (cons (+ (* num1top num2bot) (* num1bot num2top)) (* num1bot num2bot))) ;; add
-                              ((= op 2) (cons (* num1top num2top) (* num1bot num2bot))) ;; multiply
-                              ;; -----------------------
-                              ;; INSERT YOUR CODE HERE 
-                              ;; -----------------------
-
-
-                              ;; ----------------------- 
-                            ))))))))
       (zero?-exp (exp1)
                  (let ((val1 (value-of exp1 env)))
                    (let ((num1 (expval->rational val1)))
                      (if (number? num1)
-                        (if (zero? num1)
-                          (bool-val #t)
-                          (bool-val #f))
-                          ;; -----------------------
-                          ;; INSERT YOUR CODE HERE 
-                          ;; -----------------------
+                         (if (zero? num1)
+                             (bool-val #t)
+                             (bool-val #f))
+                         ;; -----------------------
+                         ;; INSERT YOUR CODE HERE 
+                         ;; -----------------------
+                         (if (zero? (car num1))
+                             (bool-val #t)
+                             (bool-val #f))))))
 
-
-                          ;; ----------------------- 
-                        ))))
-
-      
+      ;; ----------------------- 
 
       (let-exp (var exp1 body)       
                (let ((val1 (value-of exp1 env)))
@@ -123,20 +121,39 @@
       ;; INSERT YOUR CODE HERE 
       ;; -----------------------
       (list-exp ()
-       (list-val '()))
+                (list-val '()))
       
       (cons-exp (exp1 lst)
-       (let ((val1 (value-of exp1 env))
-             (lst-val (value-of lst env)))
-         (let ((num1 (expval->num val1))
-               (lst1 (expval->list lst-val)))
-           (list-val (cons num1 lst1)))))
+                (let ((val1 (value-of exp1 env))
+                      (lst-val (value-of lst env)))
+                  (let ((num1 (expval->num val1))
+                        (lst1 (expval->list lst-val)))
+                    (list-val (cons num1 lst1)))))
+
       (mul-exp (lst)
-       (let ((val1 (value-of lst env)))
-         (let ((lst1 (expval->list val1)))
-           (num-val (mul-list-value-of-helper lst1)))))
-      
-                
+               (let ((val1 (value-of lst env)))
+                 (let ((lst1 (expval->list val1)))
+                   (num-val (mul-list-value-of-helper lst1)))))
+
+      (min-exp (lst)
+               (let ((val1 (value-of lst env)))
+                 (let ((lst1 (expval->list val1)))
+                   (let ((result (find-min-in-list lst1)))
+                     (num-val result)))))
+
+      (if-elif-exp (exp1 exp2 exp3 exp4 exp5)
+                   (let ((val1 (value-of exp1 env)))
+                     (if (expval->bool val1)          
+                         (value-of exp2 env)         
+                         (let ((val3 (value-of exp3 env))) 
+                           (if (expval->bool val3)
+                               (value-of exp4 env)    
+                               (value-of exp5 env))))))
+
+      (rational-exp (num1 num2)
+                    (if (zero? num2)
+                        (eopl:error 'value-of "Denominator cannot be zero: ~s" num2)
+                        (rational-val (cons num1 num2))))
 
       ;; -----------------------
 
@@ -145,7 +162,12 @@
 (define mul-list-value-of-helper
   (lambda (lst)
     (cond
-       ((null? lst) 0)
-       ((null? (cdr lst)) (car lst))
-       (else (* (car lst) (mul-list-value-of-helper (cdr lst)))))))
-       
+      ((null? lst) 0)
+      ((null? (cdr lst)) (car lst))
+      (else (* (car lst) (mul-list-value-of-helper (cdr lst)))))))
+
+(define find-min-in-list
+  (lambda (lst)
+    (if (null? lst)
+        -1
+        (apply min lst))))
